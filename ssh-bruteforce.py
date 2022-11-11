608f222be38c3c77b154c26ebaa3f353aa9c05aa
internetlandlord <f.fredhenry@gmail.com>
2022-11-11 17:43:22 +0000

    updated with CLI arguments instead of hard-coded variables

--- ssh-bruteforce.py
+++ ssh-bruteforce.py
@@ -16,23 +16,31 @@ import sys
 
 #print("test of bfssh")
 
+# ---------- Input Validation
+if len(sys.argv) != 4:
+    print("Invalid argument.")
+    print(" >> {} <IPv4 Address> <Username> <path/to/wordlist>".format(sys.argv[0]))
+    exit()
+
 # target host IP address
 #FIXME: convert this into a sys.argv[2]
-host = "127.0.0.1"
+host = sys.argv[1]
+#host = "127.0.0.1"
 
 # target credentials - username
 #FIXME: convert into sys.argv[3] OR as an overloaded function
-username = "notroot"
+username = sys.argv[2]
+#username = "notroot"
 
 # wordlist used
 #FIXME: add as sys.argv[4]
-# wordlist = sys.argv[4]
+wordlist = sys.argv[3]
 
 # used for keeping track of attempts
 attempts = 0
 
 # opening a file to read, and using loop to operate on contents
-with open("ssh-common-passwords.txt", 'r') as password_list: #FIXME: replace with wordlist once active
+with open(wordlist, 'r') as password_list: #FIXME: replace with wordlist once active
     for password in password_list:
         # removing unneeded newlines from passwords
         password = password.strip("\n")
